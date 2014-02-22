16e6b8a75d5891d5d8cad2fa1bc1945053690462
Timothy Hermida <tim.hermida@gmail.com>
2014-02-22 15:50:40 +0000

    initialised commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SoftwareHut5::Application.config.secret_key_base = '22e99e49e8ae7196b0731b877182e02fe2da543a939f287a1d4af6ae0b8e68949369dd32254aae5a129815ecd81dbfccac44e9dc7a3776badc1332ded8f10ff7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SoftwareHut5::Application.config.session_store :cookie_store, key: '_software_hut_5_session'
